d24a1d79380feb2e77c2dc61b807bec2e912b3c4
AatifM <119080567+AatifM@users.noreply.github.com>
2025-04-29 14:06:09 -0400

    Create BookService.java
    Created the service layer for interaction.

---
+++ src/main/java/com/aatif/book_review_api/service/BookService.java
@@ -0,0 +1,37 @@
+package com.aatif.book_review_api.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aatif.book_review_api.model.Book;
+import com.aatif.book_review_api.repository.BookRepository;
+
+@Service
+public class BookService {
+    
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
