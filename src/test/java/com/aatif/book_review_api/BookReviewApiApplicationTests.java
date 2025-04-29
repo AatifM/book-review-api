e1185af42792d7bd8549343ab2027789a0e84f78
AatifM <119080567+AatifM@users.noreply.github.com>
2025-04-29 10:07:12 -0400

    Initial commit

---
+++ src/test/java/com/aatif/book_review_api/BookReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aatif.book_review_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
