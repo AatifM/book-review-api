258a76b0784aa18169419c0b114514931a524681
AatifM <119080567+AatifM@users.noreply.github.com>
2025-04-29 11:00:23 -0400

    Update Book.java
    Used Lombok to clean up boilerplate

--- src/main/java/com/aatif/book_review_api/model/Book.java
+++ src/main/java/com/aatif/book_review_api/model/Book.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,28 +23,4 @@ public class Book {
     private String author;
     private String isbn;
     private String description;
-
-    // Default contructor required by JPA
-    public Book() {}
-
-    public Book(String title, String author, String isbn, String description) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.description = description;
-    }
-
-    public Long getId() { return id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-
-    public String getIsbn() { return isbn; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
 }
